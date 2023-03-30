dd02663e7d703bac42ec5d63a167ead4d6528044
truonggiang2512 <hanguyentruonggiang004@gmail.com>
2023-03-30 13:37:14 +0700

    merge index

---
+++ controllers/index.js
@@ -0,0 +1,70 @@
+function renderTableItem(arrProduct) {
+  var htmlString = "";
+  for (var index = 0; index < arrProduct.length; index++) {
+    var item = arrProduct[index];
+    htmlString += `
+                <div class=" col-6 col-xl-3 col-lg-4 col-sm-6  ">
+                <div class="docute ">
+              <div class="product-top ">
+              <a href="" class="product-thumb"><img src="${item.image}" alt="" /></a>
+              </div>
+              <div class="product-info">
+                <a class="product-name">${item.name}</a>
+                <p class="product-description">${item.shortDescription}</p>
+                <a href="../detail.html" class="text-danger">Buy now</a>  
+                <div class="product-price mt-2">${item.price}$</div>
+                </div>
+              </div>
+            </div>`;
+  }
+  document.getElementById("item").innerHTML = htmlString;
+  return htmlString;
+}
+
+function renderTableCarousel(carousel) {
+  htmlString = `
+      <div class="detail my-auto">
+                    <h1>${carousel.name}</h1>
+                    <p>${carousel.shortDescription}</p>
+                    <div class="Buying">
+                      <button class="btn-man">
+                        <a href="#" title="Buy for Man"
+                          >Buy For Man <i class="fa-solid fa-arrow-right"></i
+                        ></a>
+                      </button>
+                      <button class="btn-man">
+                        <a href="#" title="Buy for Woman"
+                          >Buy For Woman <i class="fa-solid fa-arrow-right"></i
+                        ></a>
+                      </button>
+                    </div>
+                  </div>`;
+
+  document.getElementById("carousel").innerHTML = htmlString;
+}
+function getData() {
+  var promise = axios({
+    url: "https://shop.cyberlearn.vn/api/Product",
+    method: "GET",
+    responseType: "json",
+  });
+  promise.then(function (res) {
+    console.log(res.data.content);
+
+    renderTableItem(res.data.content);
+  });
+}
+
+function getDataCarousel() {
+  var promise = axios({
+    url: "https://shop.cyberlearn.vn/api/Product/getbyid?id=1",
+    method: "GET",
+    responseType: "json",
+  });
+  promise.then(function (res) {
+    renderTableCarousel(res.data.content);
+  });
+}
+
+getData();
+getDataCarousel();
